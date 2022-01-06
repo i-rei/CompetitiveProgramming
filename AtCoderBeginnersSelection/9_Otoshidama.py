594b58288fec129c79914334c979e5554a708808
i_rei <kahow46@gmail.com>
2022-01-06 20:46:42 +0900

    push Otoshidama.But this code result is TLE

---
+++ AtCoderBeginnersSelection/9_Otoshidama.py
@@ -0,0 +1,11 @@
+n, t = map(int, input().split())
+
+for i in range(n + 1):
+    for j in range(n + 1 - i):
+        for k in range(n + 1 - i - j):
+            r = 10000 * i + 5000 * j + 1000 * k
+            if r == t and i + j + k == n:
+                print("{} {} {}".format(i, j, k))
+                exit()
+
+print("-1 -1 -1")
