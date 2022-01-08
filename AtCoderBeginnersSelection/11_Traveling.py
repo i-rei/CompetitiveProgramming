e5c04f86d30a1970d0919652c872d491df47c765
i_rei <kahow46@gmail.com>
2022-01-08 20:23:57 +0900

    push

---
+++ AtCoderBeginnersSelection/11_Traveling.py
@@ -0,0 +1,7 @@
+n = int(input())
+a = [list(map(int, input().split())) for i in range(n)]
+
+for i in a:
+    r = [0, 0]
+    for j in range(i[0]):
+        
--- practice/WelcomeToAtCoder.py
+++
@@ -1,5 +0,0 @@
-a = int(input())
-b, c = map(int, input().split())
-t = input()
-
-print(a+b+c, t)
