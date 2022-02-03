d791f913da2191b1276dc6b3979ce72dde218b4d
i_rei <kahow46@gmail.com>
2022-02-03 20:57:44 +0900

    there is a error. but there is not grammer mistake. why?

--- ABC234/B_LongestSegment.py
+++ ABC234/B_LongestSegment.py
@@ -1,17 +1,16 @@
 import math
 n = int(input())
 a, b = [], []
-result = 0
 
 for i in range(n):
     m, n = map(int, input().split())
     a.append(m)
     b.append(n)
-
+result = 0
 for j in range(n):
-    for k in range(j + 1, n):
-        m = a[j] - a[k]
-        n = b[j] - b[k]
+    for i in range(j+1, n):
+        m = a[j] - a[i]
+        n = b[j] - b[i]
         print(m, n)
         result = max(result, math.sqrt(m * m + n * n))
 
