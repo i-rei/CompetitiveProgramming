3b230843d93ff96f23998f3e7cce12a46c50464f
i_rei <kahow46@gmail.com>
2022-02-11 21:34:57 +0900

    this is AC

--- ABC236/B_WhoIsMissing.py
+++ ABC236/B_WhoIsMissing.py
@@ -1,6 +1,4 @@
 n = int(input())
-a = list(input().split())
-for i in range(1, n + 1):
-    if not a.count(str(i)) == 4:
-        print(i)
-        exit()
+x = list(map(int, input().split()))
+y = [i for i in range(1, n + 1) for k in range(4)]
+print(sum(y) - sum(x))
