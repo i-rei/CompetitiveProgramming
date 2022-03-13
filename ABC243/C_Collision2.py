fd9fbd87be086cc5d6d0efaf4e4b587619bbab51
i_rei <kahow46@gmail.com>
2022-03-13 18:00:15 +0900

    add ABC243 C

--- ABC243/C_Collision2.py
+++ ABC243/C_Collision2.py
@@ -1,17 +1,23 @@
-import itertools
+# 参考：https://qiita.com/sano192/items/6a9ba0ccb5b3c1538a84#c---collision-2
+from collections import defaultdict
 n = int(input())
-p = [list(map(int, input().split())) for i in range(n)]
+x = [list(map(int, input().split())) for i in range(n)]
+y = set()
+for i in x:
+    y.add(i[1])
 s = input()
-a = [i for i in itertools.combinations(p, 2)]
 
-for i in a:
-    s1 = s[p.index(i[0])]
-    s2 = s[p.index(i[1])]
-    if i[0][1] == i[1][1] and s1 != s2:
-        if i[0][0] < i[1][0] and s1 == "R":
-            print("Yes")
-            exit()
-        elif i[0][0] > i[1][0] and s1 == "L":
+L, R = defaultdict(list), defaultdict(list)
+
+for i in range(n):
+    if s[i] == "L":
+        L[x[i][1]].append(x[i][0])
+    else:
+        R[x[i][1]].append(x[i][0])
+
+for i in y:
+    if len(L[i]) > 0 and len(R[i]) > 0:
+        if max(L[i]) > min(R[i]):
             print("Yes")
             exit()
 print("No")
