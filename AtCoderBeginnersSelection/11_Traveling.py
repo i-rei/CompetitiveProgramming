f7d9924680710fb4202d7b22cda3ed20c7f4e7fd
i_rei <kahow46@gmail.com>
2022-03-13 18:17:15 +0900

    add Traveling

--- AtCoderBeginnersSelection/11_Traveling.py
+++ AtCoderBeginnersSelection/11_Traveling.py
@@ -1,7 +1,18 @@
 n = int(input())
-a = [list(map(int, input().split())) for i in range(n)]
-
+a = tuple(tuple(map(int, input().split())) for i in range(n))
+now = [0, 0, 0]
 for i in a:
-    r = [0, 0]
-    for j in range(i[0]):
-        
+    t = i[0] - now[0]
+    r = abs(now[1] - i[1]) + abs(now[2] - i[2])
+    if now[1] == i[1] and now[2] == i[2] and t % 2 == 1:
+        print("No")
+        exit()
+    elif r > t:
+        print("No")
+        exit()
+    elif (t - r) % 2 == 1:
+        print("No")
+        exit()
+    else:
+        now = i
+print("Yes")
