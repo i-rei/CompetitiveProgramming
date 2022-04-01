ff8bec05c72cb1a904eeb1e5af3b30fd50a3d57b
i_rei <kahow46@gmail.com>
2022-04-01 15:50:59 +0900

    add #25

---
+++ AtCoderProblemsTraining/Easy100/25_C_CountOrder.py
@@ -0,0 +1,8 @@
+import itertools
+n = [i + 1 for i in range(int(input()))]
+p = tuple(map(int, input().split()))
+q = tuple(map(int, input().split()))
+
+c = sorted(list(itertools.permutations(n)))
+print(c)
+print(abs(c.index(p) - c.index(q)))
