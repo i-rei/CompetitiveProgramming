3a956e75ea6be69c1c3afabf1796e77726274e02
i_rei <kahow46@gmail.com>
2022-04-03 20:16:16 +0900

    add

--- AtCoderProblemsTraining/Easy100/30_B_RuinedSquare.py
+++ AtCoderProblemsTraining/Easy100/30_B_RuinedSquare.py
@@ -1,3 +1,8 @@
-from math import sqrt
 x1, y1, x2, y2 = map(int, input().split())
-h = (x2 - x1) ** 2 + (y2 - y1) ** 2
+x = x2 - x1
+y = y2 - y1
+x3 = x2 - y
+y3 = y2 + x
+x4 = x1 - y
+y4 = y1 + x
+print(x3, y3, x4, y4)
