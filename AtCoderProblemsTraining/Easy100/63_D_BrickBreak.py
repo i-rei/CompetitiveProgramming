5034777ec666cc70a4ebc4dda75b7c7c629dfcba
i_rei <77252581+i-rei@users.noreply.github.com>
2022-04-13 20:28:40 +0900

    add 63

--- AtCoderProblemsTraining/Easy100/63_D_BrickBreak.py
+++ AtCoderProblemsTraining/Easy100/63_D_BrickBreak.py
@@ -1,25 +1,15 @@
+# 参考 https://kabukimining.hateblo.jp/entry/2019/12/23/152913
+from collections import Counter
 n = int(input())
 a = list(map(int, input().split()))
-r = []
-ri = [0]
-if len(set(a)) == 1:
-    print(-1)
-    exit()
-for i in range(max(a) + 1):
-    if i in a:
-        r.append(i)
-        w = [x for x, y in enumerate(a) if y == i]
-        print(i, w)
-        for x in w:
-            if x > max(ri):
-                ri.append(x)
-                w = []
-        print(len(w), r, ri)
-        if len(w) != 0:
-            print(n - len(r))
-            exit()
-print(r)
-if len(r) == 0:
-    print(-1)
+c = 1
+for i in range(n):
+    if a[i] == c:
+        c += 1
+    else:
+        a[i] = 0
+r = Counter(a)[0]
+if r == n:
+    print("-1")
 else:
-    print(n - len(r))
+    print(r)
