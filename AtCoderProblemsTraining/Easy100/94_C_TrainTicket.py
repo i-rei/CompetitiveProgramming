902985f7ba0b691a9e03c5488592bb3dd00ae47e
i_rei <77252581+i-rei@users.noreply.github.com>
2022-05-04 20:54:18 +0900

    add

--- AtCoderProblemsTraining/Easy100/94_C_TrainTicket.py
+++ AtCoderProblemsTraining/Easy100/94_C_TrainTicket.py
@@ -1,3 +1,15 @@
-import itertools
-n = list(map(int, input().split()))
-print(n)
+n = input()
+a = 3
+for i in range(2 ** a):
+    r = int(n[0])
+    ans = n[0]
+    for j in range(a):
+        if ((i >> j) & 1):
+            r += int(n[j + 1])
+            ans = ans + "+" + n[j + 1]
+        else:
+            r -= int(n[j + 1])
+            ans = ans + "-" + n[j + 1]
+    if r == 7:
+        print(ans + "=7")
+        exit()
